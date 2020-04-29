5d77e69b87435f5549cec45a955d27cf486a6cf5
Ayden Hull <ayden.hull@internode.on.net>
2020-04-29 13:32:43 +1000

    Only update WAC delegation on Windows Servers

--- Update-WacDelegation.ps1
+++ Update-WacDelegation.ps1
@@ -1,8 +1,10 @@
 # Allow WAC delegation
 $wacServer = Get-ADComputer -Identity "MyWacServer"
 
-$wacManagedMachines = (Get-ADGroupMember -Identity "ShadowGroup_Servers" -Recursive) + (Get-ADGroupMember -Identity "Domain Controllers")
+$wacManagedMachines = (Get-ADGroupMember -Identity "ShadowGroup_Servers" -Recursive) + (Get-ADGroupMember -Identity "Domain Controllers") | ? { ($_ | Get-ADComputer -Properties OperatingSystem).OperatingSystem.StartsWith("Windows Server") }
 
 $wacManagedMachines | % {
+	Write-Host "Allow remote administration for" $_.Name
+	
 	$_ | Set-ADComputer -PrincipalsAllowedToDelegateToAccount $wacServer
 }
